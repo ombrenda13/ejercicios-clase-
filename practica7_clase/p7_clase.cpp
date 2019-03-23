a06723a768ef38c8116c108e2705dc30f9cd5d41
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2019-03-22 23:54:53 -0600

    Add files via upload

---
+++ practica7_clase/p7_clase.cpp
@@ -0,0 +1,463 @@
+/*---------------------------------------------------------*/
+/* ----------------   Práctica 7 --------------------------*/
+/*-----------------    2019-2   ---------------------------*/
+/*------------- Alumno: ORTIZ MARTÍNEZ BRENDA GRUPO:4 
+
+instrucciones: el mouse es la cámara. 
+tecla s-- aleja camara
+tecla w ..acerca camada
+tecla d .. movimiento derecha
+tecla a.. movimiento izquieda
+
+---------------*/
+#include "esfera.h"
+#include "camera.h"
+
+Esfera my_sphere(1.0);
+
+void resize(GLFWwindow* window, int width, int height);
+void my_input(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+
+// settings
+// Window size
+int SCR_WIDTH = 3800;
+int SCR_HEIGHT = 7600;
+
+GLFWmonitor *monitors;
+GLuint VBO, VAO, lightVAO;
+
+//Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+double	lastX = 0.0f,
+lastY = 0.0f;
+bool firstMouse = true;
+
+//Timing
+double	deltaTime = 0.0f,
+lastFrame = 0.0f;
+
+//Lighting
+glm::vec3 lightPos(5.0f, 5.0f, 10.0f);
+
+void myData(void);
+void display(void);
+void getResolution(void);
+void animate(void);
+
+//For Keyboard
+float	movX = 0.0f,
+movY = 0.0f,
+movZ = -5.0f,
+rotX = 0.0f;
+
+float	sol = 0.0f,
+mercurio = 0.0f,
+venus = 0.0f,
+tierra = 0.0f,
+marte = 0.0f,
+jupiter = 0.0f,
+saturno = 0.0f,
+urano = 0.0f,
+neptuno = 0.0f,
+luna_tierra = 0.0f,
+luna1_marte = 0.0f,
+luna2_marte = 0.0f,
+lun_ju_1 = 0.0f,
+lun_ju_2 = 0.0f,
+lun_ju_3 = 0.0f,
+lun_ju_4 = 0.0f,
+lun_urano1 = 0.0f,
+lun_urano2 = 0.0f
+
+;
+
+
+void getResolution()
+{
+	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	SCR_WIDTH = mode->width;
+	SCR_HEIGHT = (mode->height) - 80;
+
+	lastX = SCR_WIDTH / 2.0f;
+	lastY = SCR_HEIGHT / 2.0f;
+
+}
+
+void myData()
+{
+	GLfloat vertices[] = {
+		//Position				//Normals
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,//
+		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	//To configure Second Objecto to represet Light
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+}
+
+void animate(void)
+{
+	sol += 0.5f;
+	mercurio += 0.8f;
+	venus += 0.7f;
+	tierra += 0.6f,
+		luna_tierra += 8.0,
+		marte += 0.5,
+		luna1_marte += 5.0,
+		luna2_marte += 2.0,
+		jupiter += 0.7,
+		lun_ju_1 += 0.4,
+		lun_ju_2 += 0.4,
+		lun_ju_3 += 0.4,
+		lun_ju_4 += 0.4,
+		saturno += 0.6,
+		urano += 0.3,
+		neptuno += 0.2,
+		lun_urano1 += 0.4,
+		lun_urano2 += 0.4
+		;
+}
+
+void display(void)
+{
+	//Shader myShader("shaders/shader.vs", "shaders/shader.fs");
+	//Shader projectionShader("shaders/shader_projection.vs", "shaders/shader_projection.fs");
+	Shader projectionShader("shaders/shader_light.vs", "shaders/shader_light.fs");
+	//Shader projectionShader("shaders/shader_light_Gouraud.vs", "shaders/shader_light_Gouraud.fs");
+	Shader lampShader("shaders/shader_lamp.vs", "shaders/shader_lamp.fs");
+
+	//To Use Lighting
+	projectionShader.use();
+	projectionShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);//definimos la componente de iluminación (color de la luz).. se multiplica la luz por el material
+	projectionShader.setVec3("lightPos", lightPos);
+
+	// create transformations and Projection
+	glm::mat4 model = glm::mat4(1.0f);		// initialize Matrix, Use this matrix for individual models
+	glm::mat4 view = glm::mat4(1.0f);		//Use this matrix for ALL models
+	glm::mat4 projection = glm::mat4(1.0f);	//This matrix is for Projection
+
+											//Use "projection" to inlcude Camera
+	projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	view = camera.GetViewMatrix();//se le asigna la camara 
+
+								  // pass them to the shaders}
+	projectionShader.setVec3("viewPos", camera.Position);
+	projectionShader.setMat4("model", model);
+	projectionShader.setMat4("view", view);
+	// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+	projectionShader.setMat4("projection", projection);
+
+
+	glBindVertexArray(VAO);
+	//Colocar código aquí
+	model = glm::rotate(model, glm::radians(sol), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 1.0f, 0.0f, 0.0f);
+	projectionShader.setVec3("diffuseColor", 1.0f, 1.0f, 0.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 1.0f, 0.0f);//intensidad 
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la mat
+	//MERCURIO
+	model = glm::rotate(model, glm::radians(mercurio), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(3, 0, 0));
+	model = glm::rotate(model, glm::radians(mercurio), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+	model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.75f, 0.0f, 1.0f);
+	projectionShader.setVec3("diffuseColor", 1.0f, 1.0f, 0.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad 
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la mat
+	//VENUS
+	model = glm::rotate(model, glm::radians(venus), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(6, 0, 0));
+	model = glm::rotate(model, glm::radians(venus), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+	model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.8f, 0.4f);
+	projectionShader.setVec3("diffuseColor", 1.0f, 1.0f, 0.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad 
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la mat
+
+	//tierra------------------------------
+	model = glm::rotate(model, glm::radians(tierra), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(9, 0, 0));
+	model = glm::rotate(model, glm::radians(tierra), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+	model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.0f, 0.8f);
+	projectionShader.setVec3("diffuseColor", 0.0f, 0.0f, 0.6f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la mat
+	//marte----------------
+	//marte-----------------------------------
+	model = glm::rotate(model, glm::radians(marte), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(12, 0, 0));
+	model = glm::rotate(model, glm::radians(marte), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+	model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.0f, 0.8f);
+	projectionShader.setVec3("diffuseColor", 1.0f, 0.0f, 0.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la m
+
+
+	//jupiter
+
+	model = glm::rotate(model, glm::radians(jupiter), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(20, 0, 0));
+	model = glm::rotate(model, glm::radians(jupiter), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.0f, 0.8f);
+	projectionShader.setVec3("diffuseColor", 0.8f, 0.03, 7.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la m
+
+
+
+	//saturno
+	model = glm::rotate(model, glm::radians(saturno), glm::vec3(0.0f, 1.0f, 0.0f));//traslacion
+	model = glm::translate(model, glm::vec3(30, 0, 0));
+	model = glm::rotate(model, glm::radians(saturno), glm::vec3(0.0f, 1.0f, 0.0f));//rotación
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.0f, 0.8f);
+	projectionShader.setVec3("diffuseColor", 0.0f, 0.5f, 0.4f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad
+	my_sphere.render();	//Sphere
+	
+ //anillo
+	model = glm::translate(model, glm::vec3(0, -0.12, 0));
+	model = glm::scale(model, glm::vec3(1.4f, 0.1f, 1.4f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("aColor", glm::vec3(1.0f, 1.0f, 1.0f));
+	projectionShader.setMat4("model", model);
+	projectionShader.setVec3("ambientColor", 0.0f, 0.0f, 0.8f);
+	projectionShader.setVec3("diffuseColor", 1.0f, 0.0f, 1.0f);
+	projectionShader.setVec3("specularColor", 1.0f, 0.0f, 0.0f);//intensidad
+	my_sphere.render();	//Sphere
+	model = glm::mat4(1.0f);//inicilizar la m
+
+
+						//glDrawArrays(GL_TRIANGLES, 0, 36);	//Light
+
+	lampShader.use();
+	lampShader.setMat4("projection", projection);
+	lampShader.setMat4("view", view);
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, lightPos);
+	model = glm::scale(model, glm::vec3(1.0f));
+	lampShader.setMat4("model", model);
+
+	glBindVertexArray(lightVAO);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);	//Light
+
+
+										//glBindVertexArray(0);
+
+}
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+														 // glfw window creation
+														 // --------------------
+	monitors = glfwGetPrimaryMonitor();
+	getResolution();
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Practica 7", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwSetWindowPos(window, 0, 30);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, resize);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	//To Enable capture of our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+	glewInit();
+
+
+	//Mis funciones
+	//Datos a utilizar
+	myData();
+	my_sphere.init();
+	glEnable(GL_DEPTH_TEST);
+
+	// render loop
+	// While the windows is not closed
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		double currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		// input
+		// -----
+		my_input(window);
+		animate();
+
+		// render
+		// Backgound color
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Mi función de dibujo
+		display();
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void my_input(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, (float)deltaTime);
+
+
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void resize(GLFWwindow* window, int width, int height)
+{
+	// Set the Viewport to the size of the created window
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	double xoffset = xpos - lastX;
+	double yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
